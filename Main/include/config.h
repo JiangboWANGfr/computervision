1e833bae060afa1eb5dcc782f6fa0fc5b6c904d0
Tan Sze-An, Peter <tanszean2000@gmail.com>
2019-12-29 23:35:20 +0800

    Remove unnecessary files.

--- Main/include/config.h
+++ Main/include/config.h
@@ -2,14 +2,14 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-14 19:02:41
- * @LastEditTime : 2019-12-29 15:34:43
+ * @LastEditTime : 2019-12-29 18:46:57
  * @LastEditors  : Please set LastEditors
  */
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// #define SENTRY
-#define BUFF
+ #define SENTRY
+//#define BUFF
 // #define INFANTRY
 // #define Hero
 // #define UAV
